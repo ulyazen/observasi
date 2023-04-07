57241f9519016ea6c24a894d89c9be9d14f1f155
Binta Wahyu Wicaksono <binta.wicaksono@bankraya.co.id>
2023-04-07 23:39:40 +0700

    add: add footer

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,10 +1,13 @@
-<script lang="ts">
-</script>
+<script lang="ts"></script>
 
 <template>
-  <div>Footer</div>
+  <footer class="bg-white text-black py-4">
+    <div class="container mx-auto flex items-center justify-center flex-col">
+      <h1 class="text-6xl font-sans">YANG SELALU DI RAWAT</h1>
+      <img src="@/assets/images/logo.svg" class="w-1/5" alt="logo" />
+      <!-- <p class="text-3xl mt-3 font-black">Observasi Kopi</p> -->
+    </div>
+  </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
