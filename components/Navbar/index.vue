fec544248bc1bab70bb404882ef7db29d6f239bc
unknown <workfatkh@gmail.com>
2023-04-07 23:49:36 +0700

    add: navbar

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,10 +1,49 @@
-<script lang="ts">
-</script>
-
 <template>
-  <div>Navbar</div>
+  <Disclosure as="nav" class="bg-light-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="sr-only">Open main menu</span>
+            
+          </DisclosureButton>
+        </div>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex flex-shrink-0 items-center justify-center">
+            <img class="block h-24 w-auto lg:hidden" src="@/assets/images/logo.svg" alt="OBSERVASI" />
+            <img class="hidden h-24 w-auto lg:block" src="@/assets/images/logo.svg" alt="OBSERVASI" />
+          </div>
+          <div class="hidden sm:ml-6 sm:flex">
+            <div class="flex items-center justify-center space-x-4">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+            </div>
+          </div>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-4">
+          <Icon  class="h-8 w-auto" name="mdi:instagram"/>
+          <Icon class="h-8 w-auto" name="mdi:twitter"/>
+          <Icon class="h-8 w-auto" name="simple-icons:tiktok"/>
+          
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
-<style scoped>
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
-</style>
+const navigation = [
+  { name: 'BERANDA', href: '#', current: true },
+  { name: 'CUITAN', href: '#', current: false },
+  { name: 'GALERI', href: '#', current: false },
+  { name: 'CERITA KOPI', href: '#', current: false },
+]
+</script>
